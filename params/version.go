4a90a4b27d51a7795f7792ebbdff412a857f8597
ethersocialnet <kimmh@genercrypto.com>
2018-04-03 17:06:00 +0900

    Version

--- params/version.go
+++ params/version.go
@@ -21,9 +21,9 @@ import (
 )
 
 const (
-	VersionMajor = 0        // Major version component of the current release
-	VersionMinor = 2        // Minor version component of the current release
-	VersionPatch = 2        // Patch version component of the current release
+	VersionMajor = 0          // Major version component of the current release
+	VersionMinor = 3          // Minor version component of the current release
+	VersionPatch = 0          // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
