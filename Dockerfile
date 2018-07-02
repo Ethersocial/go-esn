luxfi/sdk

Dockerfile: use non-privileged user account (#16052)

Dockerfile
@@ -12,5 +12,11 @@ FROM alpine:latest
 RUN apk add --no-cache ca-certificates
 COPY --from=builder /go-ethereum/build/bin/geth /usr/local/bin/
 
+RUN addgroup -g 1000 geth && \
+    adduser -h /root -D -u 1000 -G geth geth && \
+    chown geth:geth /root
+
+USER geth
+
 EXPOSE 8545 8546 30303 30303/udp 30304/udp
 ENTRYPOINT ["geth"]

Ethersocial/go-esn

Dockerfile
@@ -12,5 +12,11 @@ FROM alpine:latest
 RUN apk add --no-cache ca-certificates
 COPY --from=builder /go-esn/build/bin/gesn /usr/local/bin/
 
+RUN addgroup -g 1000 gesn && \
+    adduser -h /root -D -u 1000 -G gesn gesn && \
+    chown gesn:gesn /root
+
+USER gesn
+
 EXPOSE 9545 9546 50505 50505/udp 50506/udp
 ENTRYPOINT ["gesn"]