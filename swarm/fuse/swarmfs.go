luxfi/sdk

build: enable goimports and varcheck linters (#16446)

swarm/fuse/swarmfs.go
@@ -17,9 +17,10 @@
 package fuse
 
 import (
-	"github.com/ethereum/go-ethereum/swarm/api"
 	"sync"
 	"time"
+
+	"github.com/ethereum/go-ethereum/swarm/api"
 )
 
 const (

Ethersocial/go-esn

swarm/fuse/swarmfs.go
@@ -17,9 +17,10 @@
 package fuse
 
 import (
-	"github.com/ethersocial/go-esn/swarm/api"
 	"sync"
 	"time"
+
+	"github.com/ethersocial/go-esn/swarm/api"
 )
 
 const (