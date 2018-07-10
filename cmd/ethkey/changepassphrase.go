63177cb7f9381eb5753b675f76bcd7b9ad956c28
kimmyeonghun <kimmh@genercrypto.com>
2018-07-10 16:34:03 +0900

    fix etc && geth v1.8.11

--- cmd/ethkey/changepassphrase.go
+++ cmd/ethkey/changepassphrase.go
@@ -5,8 +5,8 @@ import (
 	"io/ioutil"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/cmd/utils"
+	"github.com/ethersocial/go-esn/accounts/keystore"
+	"github.com/ethersocial/go-esn/cmd/utils"
 	"gopkg.in/urfave/cli.v1"
 )
 
--- core/tx_cacher.go
+++ core/tx_cacher.go
@@ -19,7 +19,7 @@ package core
 import (
 	"runtime"
 
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethersocial/go-esn/core/types"
 )
 
 // senderCacher is a concurrent tranaction sender recoverer anc cacher.
