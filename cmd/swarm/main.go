31cce297475db5b74060258579bac4e939cf8d7c
Anton Evangelatov <anton.evangelatov@gmail.com>
2018-07-13 17:40:28 +0200

    swarm: integrate OpenTracing; propagate ctx to internal APIs (#17169)
    * swarm: propagate ctx, enable opentracing
    
    * swarm/tracing: log error when tracing is misconfigured

--- cmd/swarm/main.go
+++ cmd/swarm/main.go
@@ -43,6 +43,7 @@ import (
 	"github.com/ethersocial/go-esn/swarm"
 	bzzapi "github.com/ethersocial/go-esn/swarm/api"
 	swarmmetrics "github.com/ethersocial/go-esn/swarm/metrics"
+	"github.com/ethersocial/go-esn/swarm/tracing"
 
 	"gopkg.in/urfave/cli.v1"
 )
@@ -430,12 +431,14 @@ pv(1) tool to get a progress bar:
 	app.Flags = append(app.Flags, rpcFlags...)
 	app.Flags = append(app.Flags, debug.Flags...)
 	app.Flags = append(app.Flags, swarmmetrics.Flags...)
+	app.Flags = append(app.Flags, tracing.Flags...)
 	app.Before = func(ctx *cli.Context) error {
 		runtime.GOMAXPROCS(runtime.NumCPU())
 		if err := debug.Setup(ctx, ""); err != nil {
 			return err
 		}
 		swarmmetrics.Setup(ctx)
+		tracing.Setup(ctx)
 		return nil
 	}
 	app.After = func(ctx *cli.Context) error {
