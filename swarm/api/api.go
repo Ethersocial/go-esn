luxfi/sdk

swarm/storage/feed: Renamed package

swarm/api/api.go
@@ -45,8 +45,8 @@ import (
 	"github.com/ethereum/go-ethereum/swarm/multihash"
 	"github.com/ethereum/go-ethereum/swarm/spancontext"
 	"github.com/ethereum/go-ethereum/swarm/storage"
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds"
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds/lookup"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed/lookup"
 
 	opentracing "github.com/opentracing/opentracing-go"
 )
@@ -236,18 +236,18 @@ on top of the FileStore
 it is the public interface of the FileStore which is included in the ethereum stack
 */
 type API struct {
-	feeds     *feeds.Handler
+	feed      *feed.Handler
 	fileStore *storage.FileStore
 	dns       Resolver
 	Decryptor func(context.Context, string) DecryptFunc
 }
 
 // NewAPI the api constructor initialises a new API instance.
-func NewAPI(fileStore *storage.FileStore, dns Resolver, feedsHandler *feeds.Handler, pk *ecdsa.PrivateKey) (self *API) {
+func NewAPI(fileStore *storage.FileStore, dns Resolver, feedHandler *feed.Handler, pk *ecdsa.PrivateKey) (self *API) {
 	self = &API{
 		fileStore: fileStore,
 		dns:       dns,
-		feeds:     feedsHandler,
+		feed:      feedHandler,
 		Decryptor: func(ctx context.Context, credentials string) DecryptFunc {
 			return self.doDecrypt(ctx, credentials, pk)
 		},
@@ -409,7 +409,7 @@ func (a *API) Get(ctx context.Context, decrypt DecryptFunc, manifestAddr storage
 			if entry.Feed == nil {
 				return reader, mimeType, status, nil, fmt.Errorf("Cannot decode Feed in manifest")
 			}
-			_, err := a.feeds.Lookup(ctx, feeds.NewQueryLatest(entry.Feed, lookup.NoClue))
+			_, err := a.feed.Lookup(ctx, feed.NewQueryLatest(entry.Feed, lookup.NoClue))
 			if err != nil {
 				apiGetNotFound.Inc(1)
 				status = http.StatusNotFound
@@ -417,7 +417,7 @@ func (a *API) Get(ctx context.Context, decrypt DecryptFunc, manifestAddr storage
 				return reader, mimeType, status, nil, err
 			}
 			// get the data of the update
-			_, rsrcData, err := a.feeds.GetContent(entry.Feed)
+			_, rsrcData, err := a.feed.GetContent(entry.Feed)
 			if err != nil {
 				apiGetNotFound.Inc(1)
 				status = http.StatusNotFound
@@ -958,13 +958,13 @@ func (a *API) BuildDirectoryTree(ctx context.Context, mhash string, nameresolver
 }
 
 // FeedsLookup finds Swarm feeds updates at specific points in time, or the latest update
-func (a *API) FeedsLookup(ctx context.Context, query *feeds.Query) ([]byte, error) {
-	_, err := a.feeds.Lookup(ctx, query)
+func (a *API) FeedsLookup(ctx context.Context, query *feed.Query) ([]byte, error) {
+	_, err := a.feed.Lookup(ctx, query)
 	if err != nil {
 		return nil, err
 	}
 	var data []byte
-	_, data, err = a.feeds.GetContent(&query.Feed)
+	_, data, err = a.feed.GetContent(&query.Feed)
 	if err != nil {
 		return nil, err
 	}
@@ -972,18 +972,18 @@ func (a *API) FeedsLookup(ctx context.Context, query *feeds.Query) ([]byte, erro
 }
 
 // FeedsNewRequest creates a Request object to update a specific feed
-func (a *API) FeedsNewRequest(ctx context.Context, feed *feeds.Feed) (*feeds.Request, error) {
-	return a.feeds.NewRequest(ctx, feed)
+func (a *API) FeedsNewRequest(ctx context.Context, feed *feed.Feed) (*feed.Request, error) {
+	return a.feed.NewRequest(ctx, feed)
 }
 
 // FeedsUpdate publishes a new update on the given feed
-func (a *API) FeedsUpdate(ctx context.Context, request *feeds.Request) (storage.Address, error) {
-	return a.feeds.Update(ctx, request)
+func (a *API) FeedsUpdate(ctx context.Context, request *feed.Request) (storage.Address, error) {
+	return a.feed.Update(ctx, request)
 }
 
 // FeedsHashSize returned the size of the digest produced by Swarm feeds' hashing function
 func (a *API) FeedsHashSize() int {
-	return a.feeds.HashSize
+	return a.feed.HashSize
 }
 
 // ErrCannotLoadFeedManifest is returned when looking up a feeds manifest fails
@@ -993,7 +993,7 @@ var ErrCannotLoadFeedManifest = errors.New("Cannot load feed manifest")
 var ErrNotAFeedManifest = errors.New("Not a feed manifest")
 
 // ResolveFeedManifest retrieves the Swarm feed manifest for the given address, and returns the referenced Feed.
-func (a *API) ResolveFeedManifest(ctx context.Context, addr storage.Address) (*feeds.Feed, error) {
+func (a *API) ResolveFeedManifest(ctx context.Context, addr storage.Address) (*feed.Feed, error) {
 	trie, err := loadManifest(ctx, a.fileStore, addr, nil, NOOPDecrypt)
 	if err != nil {
 		return nil, ErrCannotLoadFeedManifest
@@ -1016,8 +1016,8 @@ var ErrCannotResolveFeed = errors.New("Cannot resolve Feed")
 
 // ResolveFeed attempts to extract feed information out of the manifest, if provided
 // If not, it attempts to extract the feed out of a set of key-value pairs
-func (a *API) ResolveFeed(ctx context.Context, uri *URI, values feeds.Values) (*feeds.Feed, error) {
-	var feed *feeds.Feed
+func (a *API) ResolveFeed(ctx context.Context, uri *URI, values feed.Values) (*feed.Feed, error) {
+	var fd *feed.Feed
 	var err error
 	if uri.Addr != "" {
 		// resolve the content key.
@@ -1030,20 +1030,20 @@ func (a *API) ResolveFeed(ctx context.Context, uri *URI, values feeds.Values) (*
 		}
 
 		// get the Swarm feed from the manifest
-		feed, err = a.ResolveFeedManifest(ctx, manifestAddr)
+		fd, err = a.ResolveFeedManifest(ctx, manifestAddr)
 		if err != nil {
 			return nil, err
 		}
-		log.Debug("handle.get.feed: resolved", "manifestkey", manifestAddr, "feed", feed.Hex())
+		log.Debug("handle.get.feed: resolved", "manifestkey", manifestAddr, "feed", fd.Hex())
 	} else {
-		var v feeds.Feed
-		if err := v.FromValues(values); err != nil {
+		var f feed.Feed
+		if err := f.FromValues(values); err != nil {
 			return nil, ErrCannotResolveFeed
 
 		}
-		feed = &v
+		fd = &f
 	}
-	return feed, nil
+	return fd, nil
 }
 
 // MimeOctetStream default value of http Content-Type header

swarm/api/http/server_test.go
@@ -38,7 +38,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds/lookup"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed/lookup"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -48,7 +48,7 @@ import (
 	swarm "github.com/ethereum/go-ethereum/swarm/api/client"
 	"github.com/ethereum/go-ethereum/swarm/multihash"
 	"github.com/ethereum/go-ethereum/swarm/storage"
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed"
 	"github.com/ethereum/go-ethereum/swarm/testutil"
 )
 
@@ -62,12 +62,12 @@ func serverFunc(api *api.API) testutil.TestServer {
 	return NewServer(api, "")
 }
 
-func newTestSigner() (*feeds.GenericSigner, error) {
+func newTestSigner() (*feed.GenericSigner, error) {
 	privKey, err := crypto.HexToECDSA("deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef")
 	if err != nil {
 		return nil, err
 	}
-	return feeds.NewGenericSigner(privKey), nil
+	return feed.NewGenericSigner(privKey), nil
 }
 
 // test the transparent resolving of multihash-containing feed updates with bzz:// scheme
@@ -103,8 +103,8 @@ func TestBzzFeedMultihash(t *testing.T) {
 
 	log.Info("added data", "manifest", string(b), "data", common.ToHex(mh))
 
-	topic, _ := feeds.NewTopic("foo.eth", nil)
-	updateRequest := feeds.NewFirstRequest(topic)
+	topic, _ := feed.NewTopic("foo.eth", nil)
+	updateRequest := feed.NewFirstRequest(topic)
 
 	updateRequest.SetData(mh)
 
@@ -182,8 +182,8 @@ func TestBzzFeed(t *testing.T) {
 	//data for update 2
 	update2Data := []byte("foo")
 
-	topic, _ := feeds.NewTopic("foo.eth", nil)
-	updateRequest := feeds.NewFirstRequest(topic)
+	topic, _ := feed.NewTopic("foo.eth", nil)
+	updateRequest := feed.NewFirstRequest(topic)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -319,7 +319,7 @@ func TestBzzFeed(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	updateRequest = &feeds.Request{}
+	updateRequest = &feed.Request{}
 	if err = updateRequest.UnmarshalJSON(b); err != nil {
 		t.Fatalf("Error decoding feed metadata: %s", err)
 	}
@@ -365,7 +365,7 @@ func TestBzzFeed(t *testing.T) {
 
 	// test manifest-less queries
 	log.Info("get first update in update1Timestamp via direct query")
-	query := feeds.NewQuery(&updateRequest.Feed, update1Timestamp, lookup.NoClue)
+	query := feed.NewQuery(&updateRequest.Feed, update1Timestamp, lookup.NoClue)
 
 	urlq, err := url.Parse(fmt.Sprintf("%s/bzz-feed:/", srv.URL))
 	if err != nil {

swarm/api/manifest.go
@@ -27,7 +27,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/swarm/log"
@@ -56,7 +56,7 @@ type ManifestEntry struct {
 	ModTime     time.Time    `json:"mod_time,omitempty"`
 	Status      int          `json:"status,omitempty"`
 	Access      *AccessEntry `json:"access,omitempty"`
-	Feed        *feeds.Feed  `json:"feed,omitempty"`
+	Feed        *feed.Feed   `json:"feed,omitempty"`
 }
 
 // ManifestList represents the result of listing files in a manifest
@@ -82,7 +82,7 @@ func (a *API) NewManifest(ctx context.Context, toEncrypt bool) (storage.Address,
 
 // Manifest hack for supporting Swarm feeds from the bzz: scheme
 // see swarm/api/api.go:API.Get() for more information
-func (a *API) NewFeedManifest(ctx context.Context, feed *feeds.Feed) (storage.Address, error) {
+func (a *API) NewFeedManifest(ctx context.Context, feed *feed.Feed) (storage.Address, error) {
 	var manifest Manifest
 	entry := ManifestEntry{
 		Feed:        feed,

swarm/storage/feed/handler.go
@@ -16,7 +16,7 @@
 
 // Handler is the API for feeds
 // It enables creating, updating, syncing and retrieving feed updates and their data
-package feeds
+package feed
 
 import (
 	"bytes"
@@ -25,7 +25,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds/lookup"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed/lookup"
 
 	"github.com/ethereum/go-ethereum/swarm/log"
 	"github.com/ethereum/go-ethereum/swarm/storage"

swarm/storage/feed/id_test.go
@@ -1,9 +1,9 @@
-package feeds
+package feed
 
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds/lookup"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed/lookup"
 )
 
 func getTestID() *ID {

swarm/storage/feed/request.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package feeds
+package feed
 
 import (
 	"bytes"
@@ -24,7 +24,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/swarm/storage"
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds/lookup"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed/lookup"
 )
 
 // Request represents a request to sign or signed feed update message

swarm/storage/feed/request_test.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package feeds
+package feed
 
 import (
 	"bytes"
@@ -26,7 +26,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/swarm/storage"
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds/lookup"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed/lookup"
 )
 
 func areEqualJSON(s1, s2 string) (bool, error) {

swarm/swarm.go
@@ -49,8 +49,8 @@ import (
 	"github.com/ethereum/go-ethereum/swarm/pss"
 	"github.com/ethereum/go-ethereum/swarm/state"
 	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/ethereum/go-ethereum/swarm/storage/feed"
 	"github.com/ethereum/go-ethereum/swarm/storage/mock"
-	"github.com/ethereum/go-ethereum/swarm/storage/feeds"
 	"github.com/ethereum/go-ethereum/swarm/tracing"
 )
 
@@ -186,10 +186,10 @@ func NewSwarm(config *api.Config, mockStore *mock.NodeStore) (self *Swarm, err e
 	// Swarm Hash Merklised Chunking for Arbitrary-length Document/File storage
 	self.fileStore = storage.NewFileStore(self.netStore, self.config.FileStoreParams)
 
-	var feedsHandler *feeds.Handler
-	fhParams := &feeds.HandlerParams{}
+	var feedsHandler *feed.Handler
+	fhParams := &feed.HandlerParams{}
 
-	feedsHandler = feeds.NewHandler(fhParams)
+	feedsHandler = feed.NewHandler(fhParams)
 	feedsHandler.SetStore(self.netStore)
 
 	lstore.Validators = []storage.ChunkValidator{

Ethersocial/go-esn

swarm/api/api.go
@@ -45,8 +45,8 @@ import (
 	"github.com/ethersocial/go-esn/swarm/multihash"
 	"github.com/ethersocial/go-esn/swarm/spancontext"
 	"github.com/ethersocial/go-esn/swarm/storage"
-	"github.com/ethersocial/go-esn/swarm/storage/feeds"
-	"github.com/ethersocial/go-esn/swarm/storage/feeds/lookup"
+	"github.com/ethersocial/go-esn/swarm/storage/feed"
+	"github.com/ethersocial/go-esn/swarm/storage/feed/lookup"
 
 	opentracing "github.com/opentracing/opentracing-go"
 )
@@ -236,18 +236,18 @@ on top of the FileStore
 it is the public interface of the FileStore which is included in the ethereum stack
 */
 type API struct {
-	feeds     *feeds.Handler
+	feed      *feed.Handler
 	fileStore *storage.FileStore
 	dns       Resolver
 	Decryptor func(context.Context, string) DecryptFunc
 }
 
 // NewAPI the api constructor initialises a new API instance.
-func NewAPI(fileStore *storage.FileStore, dns Resolver, feedsHandler *feeds.Handler, pk *ecdsa.PrivateKey) (self *API) {
+func NewAPI(fileStore *storage.FileStore, dns Resolver, feedHandler *feed.Handler, pk *ecdsa.PrivateKey) (self *API) {
 	self = &API{
 		fileStore: fileStore,
 		dns:       dns,
-		feeds:     feedsHandler,
+		feed:      feedHandler,
 		Decryptor: func(ctx context.Context, credentials string) DecryptFunc {
 			return self.doDecrypt(ctx, credentials, pk)
 		},
@@ -409,7 +409,7 @@ func (a *API) Get(ctx context.Context, decrypt DecryptFunc, manifestAddr storage
 			if entry.Feed == nil {
 				return reader, mimeType, status, nil, fmt.Errorf("Cannot decode Feed in manifest")
 			}
-			_, err := a.feeds.Lookup(ctx, feeds.NewQueryLatest(entry.Feed, lookup.NoClue))
+			_, err := a.feed.Lookup(ctx, feed.NewQueryLatest(entry.Feed, lookup.NoClue))
 			if err != nil {
 				apiGetNotFound.Inc(1)
 				status = http.StatusNotFound
@@ -417,7 +417,7 @@ func (a *API) Get(ctx context.Context, decrypt DecryptFunc, manifestAddr storage
 				return reader, mimeType, status, nil, err
 			}
 			// get the data of the update
-			_, rsrcData, err := a.feeds.GetContent(entry.Feed)
+			_, rsrcData, err := a.feed.GetContent(entry.Feed)
 			if err != nil {
 				apiGetNotFound.Inc(1)
 				status = http.StatusNotFound
@@ -958,13 +958,13 @@ func (a *API) BuildDirectoryTree(ctx context.Context, mhash string, nameresolver
 }
 
 // FeedsLookup finds Swarm feeds updates at specific points in time, or the latest update
-func (a *API) FeedsLookup(ctx context.Context, query *feeds.Query) ([]byte, error) {
-	_, err := a.feeds.Lookup(ctx, query)
+func (a *API) FeedsLookup(ctx context.Context, query *feed.Query) ([]byte, error) {
+	_, err := a.feed.Lookup(ctx, query)
 	if err != nil {
 		return nil, err
 	}
 	var data []byte
-	_, data, err = a.feeds.GetContent(&query.Feed)
+	_, data, err = a.feed.GetContent(&query.Feed)
 	if err != nil {
 		return nil, err
 	}
@@ -972,18 +972,18 @@ func (a *API) FeedsLookup(ctx context.Context, query *feeds.Query) ([]byte, erro
 }
 
 // FeedsNewRequest creates a Request object to update a specific feed
-func (a *API) FeedsNewRequest(ctx context.Context, feed *feeds.Feed) (*feeds.Request, error) {
-	return a.feeds.NewRequest(ctx, feed)
+func (a *API) FeedsNewRequest(ctx context.Context, feed *feed.Feed) (*feed.Request, error) {
+	return a.feed.NewRequest(ctx, feed)
 }
 
 // FeedsUpdate publishes a new update on the given feed
-func (a *API) FeedsUpdate(ctx context.Context, request *feeds.Request) (storage.Address, error) {
-	return a.feeds.Update(ctx, request)
+func (a *API) FeedsUpdate(ctx context.Context, request *feed.Request) (storage.Address, error) {
+	return a.feed.Update(ctx, request)
 }
 
 // FeedsHashSize returned the size of the digest produced by Swarm feeds' hashing function
 func (a *API) FeedsHashSize() int {
-	return a.feeds.HashSize
+	return a.feed.HashSize
 }
 
 // ErrCannotLoadFeedManifest is returned when looking up a feeds manifest fails
@@ -993,7 +993,7 @@ var ErrCannotLoadFeedManifest = errors.New("Cannot load feed manifest")
 var ErrNotAFeedManifest = errors.New("Not a feed manifest")
 
 // ResolveFeedManifest retrieves the Swarm feed manifest for the given address, and returns the referenced Feed.
-func (a *API) ResolveFeedManifest(ctx context.Context, addr storage.Address) (*feeds.Feed, error) {
+func (a *API) ResolveFeedManifest(ctx context.Context, addr storage.Address) (*feed.Feed, error) {
 	trie, err := loadManifest(ctx, a.fileStore, addr, nil, NOOPDecrypt)
 	if err != nil {
 		return nil, ErrCannotLoadFeedManifest
@@ -1016,8 +1016,8 @@ var ErrCannotResolveFeed = errors.New("Cannot resolve Feed")
 
 // ResolveFeed attempts to extract feed information out of the manifest, if provided
 // If not, it attempts to extract the feed out of a set of key-value pairs
-func (a *API) ResolveFeed(ctx context.Context, uri *URI, values feeds.Values) (*feeds.Feed, error) {
-	var feed *feeds.Feed
+func (a *API) ResolveFeed(ctx context.Context, uri *URI, values feed.Values) (*feed.Feed, error) {
+	var fd *feed.Feed
 	var err error
 	if uri.Addr != "" {
 		// resolve the content key.
@@ -1030,20 +1030,20 @@ func (a *API) ResolveFeed(ctx context.Context, uri *URI, values feeds.Values) (*
 		}
 
 		// get the Swarm feed from the manifest
-		feed, err = a.ResolveFeedManifest(ctx, manifestAddr)
+		fd, err = a.ResolveFeedManifest(ctx, manifestAddr)
 		if err != nil {
 			return nil, err
 		}
-		log.Debug("handle.get.feed: resolved", "manifestkey", manifestAddr, "feed", feed.Hex())
+		log.Debug("handle.get.feed: resolved", "manifestkey", manifestAddr, "feed", fd.Hex())
 	} else {
-		var v feeds.Feed
-		if err := v.FromValues(values); err != nil {
+		var f feed.Feed
+		if err := f.FromValues(values); err != nil {
 			return nil, ErrCannotResolveFeed
 
 		}
-		feed = &v
+		fd = &f
 	}
-	return feed, nil
+	return fd, nil
 }
 
 // MimeOctetStream default value of http Content-Type header

swarm/api/http/server_test.go
@@ -38,7 +38,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethersocial/go-esn/swarm/storage/feeds/lookup"
+	"github.com/ethersocial/go-esn/swarm/storage/feed/lookup"
 
 	"github.com/ethersocial/go-esn/common"
 	"github.com/ethersocial/go-esn/core/types"
@@ -48,7 +48,7 @@ import (
 	swarm "github.com/ethersocial/go-esn/swarm/api/client"
 	"github.com/ethersocial/go-esn/swarm/multihash"
 	"github.com/ethersocial/go-esn/swarm/storage"
-	"github.com/ethersocial/go-esn/swarm/storage/feeds"
+	"github.com/ethersocial/go-esn/swarm/storage/feed"
 	"github.com/ethersocial/go-esn/swarm/testutil"
 )
 
@@ -62,12 +62,12 @@ func serverFunc(api *api.API) testutil.TestServer {
 	return NewServer(api, "")
 }
 
-func newTestSigner() (*feeds.GenericSigner, error) {
+func newTestSigner() (*feed.GenericSigner, error) {
 	privKey, err := crypto.HexToECDSA("deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef")
 	if err != nil {
 		return nil, err
 	}
-	return feeds.NewGenericSigner(privKey), nil
+	return feed.NewGenericSigner(privKey), nil
 }
 
 // test the transparent resolving of multihash-containing feed updates with bzz:// scheme
@@ -103,8 +103,8 @@ func TestBzzFeedMultihash(t *testing.T) {
 
 	log.Info("added data", "manifest", string(b), "data", common.ToHex(mh))
 
-	topic, _ := feeds.NewTopic("foo.eth", nil)
-	updateRequest := feeds.NewFirstRequest(topic)
+	topic, _ := feed.NewTopic("foo.eth", nil)
+	updateRequest := feed.NewFirstRequest(topic)
 
 	updateRequest.SetData(mh)
 
@@ -182,8 +182,8 @@ func TestBzzFeed(t *testing.T) {
 	//data for update 2
 	update2Data := []byte("foo")
 
-	topic, _ := feeds.NewTopic("foo.eth", nil)
-	updateRequest := feeds.NewFirstRequest(topic)
+	topic, _ := feed.NewTopic("foo.eth", nil)
+	updateRequest := feed.NewFirstRequest(topic)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -319,7 +319,7 @@ func TestBzzFeed(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	updateRequest = &feeds.Request{}
+	updateRequest = &feed.Request{}
 	if err = updateRequest.UnmarshalJSON(b); err != nil {
 		t.Fatalf("Error decoding feed metadata: %s", err)
 	}
@@ -365,7 +365,7 @@ func TestBzzFeed(t *testing.T) {
 
 	// test manifest-less queries
 	log.Info("get first update in update1Timestamp via direct query")
-	query := feeds.NewQuery(&updateRequest.Feed, update1Timestamp, lookup.NoClue)
+	query := feed.NewQuery(&updateRequest.Feed, update1Timestamp, lookup.NoClue)
 
 	urlq, err := url.Parse(fmt.Sprintf("%s/bzz-feed:/", srv.URL))
 	if err != nil {

swarm/api/manifest.go
@@ -27,7 +27,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethersocial/go-esn/swarm/storage/feeds"
+	"github.com/ethersocial/go-esn/swarm/storage/feed"
 
 	"github.com/ethersocial/go-esn/common"
 	"github.com/ethersocial/go-esn/swarm/log"
@@ -56,7 +56,7 @@ type ManifestEntry struct {
 	ModTime     time.Time    `json:"mod_time,omitempty"`
 	Status      int          `json:"status,omitempty"`
 	Access      *AccessEntry `json:"access,omitempty"`
-	Feed        *feeds.Feed  `json:"feed,omitempty"`
+	Feed        *feed.Feed   `json:"feed,omitempty"`
 }
 
 // ManifestList represents the result of listing files in a manifest
@@ -82,7 +82,7 @@ func (a *API) NewManifest(ctx context.Context, toEncrypt bool) (storage.Address,
 
 // Manifest hack for supporting Swarm feeds from the bzz: scheme
 // see swarm/api/api.go:API.Get() for more information
-func (a *API) NewFeedManifest(ctx context.Context, feed *feeds.Feed) (storage.Address, error) {
+func (a *API) NewFeedManifest(ctx context.Context, feed *feed.Feed) (storage.Address, error) {
 	var manifest Manifest
 	entry := ManifestEntry{
 		Feed:        feed,

swarm/storage/feed/handler.go
@@ -16,7 +16,7 @@
 
 // Handler is the API for feeds
 // It enables creating, updating, syncing and retrieving feed updates and their data
-package feeds
+package feed
 
 import (
 	"bytes"
@@ -25,7 +25,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethersocial/go-esn/swarm/storage/feeds/lookup"
+	"github.com/ethersocial/go-esn/swarm/storage/feed/lookup"
 
 	"github.com/ethersocial/go-esn/swarm/log"
 	"github.com/ethersocial/go-esn/swarm/storage"

swarm/storage/feed/id_test.go
@@ -1,9 +1,9 @@
-package feeds
+package feed
 
 import (
 	"testing"
 
-	"github.com/ethersocial/go-esn/swarm/storage/feeds/lookup"
+	"github.com/ethersocial/go-esn/swarm/storage/feed/lookup"
 )
 
 func getTestID() *ID {

swarm/storage/feed/request.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package feeds
+package feed
 
 import (
 	"bytes"
@@ -24,7 +24,7 @@ import (
 	"github.com/ethersocial/go-esn/common"
 	"github.com/ethersocial/go-esn/common/hexutil"
 	"github.com/ethersocial/go-esn/swarm/storage"
-	"github.com/ethersocial/go-esn/swarm/storage/feeds/lookup"
+	"github.com/ethersocial/go-esn/swarm/storage/feed/lookup"
 )
 
 // Request represents a request to sign or signed feed update message

swarm/storage/feed/request_test.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package feeds
+package feed
 
 import (
 	"bytes"
@@ -26,7 +26,7 @@ import (
 
 	"github.com/ethersocial/go-esn/crypto"
 	"github.com/ethersocial/go-esn/swarm/storage"
-	"github.com/ethersocial/go-esn/swarm/storage/feeds/lookup"
+	"github.com/ethersocial/go-esn/swarm/storage/feed/lookup"
 )
 
 func areEqualJSON(s1, s2 string) (bool, error) {

swarm/swarm.go
@@ -49,8 +49,8 @@ import (
 	"github.com/ethersocial/go-esn/swarm/pss"
 	"github.com/ethersocial/go-esn/swarm/state"
 	"github.com/ethersocial/go-esn/swarm/storage"
+	"github.com/ethersocial/go-esn/swarm/storage/feed"
 	"github.com/ethersocial/go-esn/swarm/storage/mock"
-	"github.com/ethersocial/go-esn/swarm/storage/feeds"
 	"github.com/ethersocial/go-esn/swarm/tracing"
 )
 
@@ -186,10 +186,10 @@ func NewSwarm(config *api.Config, mockStore *mock.NodeStore) (self *Swarm, err e
 	// Swarm Hash Merklised Chunking for Arbitrary-length Document/File storage
 	self.fileStore = storage.NewFileStore(self.netStore, self.config.FileStoreParams)
 
-	var feedsHandler *feeds.Handler
-	fhParams := &feeds.HandlerParams{}
+	var feedsHandler *feed.Handler
+	fhParams := &feed.HandlerParams{}
 
-	feedsHandler = feeds.NewHandler(fhParams)
+	feedsHandler = feed.NewHandler(fhParams)
 	feedsHandler.SetStore(self.netStore)
 
 	lstore.Validators = []storage.ChunkValidator{